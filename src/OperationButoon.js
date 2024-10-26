35be73ba9a507c2ab6ffa3cdb05f6f8deb3a3354
Godawela <godawelaus.21@uom.lk>
2024-10-26 15:33:50 +0530

    final funtions

--- src/OperationButoon.js
+++ src/OperationButoon.js
@@ -1,6 +1,6 @@
 import { ACTIONS } from './App';
 
-export default function OperationButton({ dispatch, operation }) {
+export default function OperationButoon({ dispatch, operation }) {
     return (
         <button 
             onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}
